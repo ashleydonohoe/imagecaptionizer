bfaa459ef426d49d472ab88f4d9c68e1bf45ea92
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-08-12 00:20:54 -0400

    ssss

---
+++ CaptionShare/CaptionedImage.swift
@@ -0,0 +1,22 @@
+//
+//  CaptionedImage.swift
+//  CaptionShare
+//
+//  Created by Gabriele on 8/12/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import Foundation
+
+
+struct Caption {
+    let text: String
+    let image: UIImage
+    let captionedImage: UIImage
+    
+    init(text: String, image: UIImage, captionedImage: UIImage) {
+        self.text = text
+        self.image = image
+        self.captionedImage = captionedImage
+    }
+}
