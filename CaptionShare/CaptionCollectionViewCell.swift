907cc28881c7a9ede399b9416cb303a9b5d1574c
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-08-15 23:30:03 -0400

    Added collectionview

---
+++ CaptionShare/CaptionCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CaptionCollectionViewCell.swift
+//  CaptionShare
+//
+//  Created by Gabriele on 8/15/16.
+//  Copyright © 2016 Ashley Donohoe. All rights reserved.
+//
+
+import UIKit
+
+class CaptionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var captionImageView: UIImageView!
+}
--- CaptionShare/CaptionCollectionViewController.swift
+++ CaptionShare/CaptionCollectionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CaptionCollectionViewController: UICollectionViewController {
     
+    @IBOutlet var captionCollectionView: UICollectionView!
     var captions: [CaptionedImage]!
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
 
@@ -19,12 +20,13 @@ class CaptionCollectionViewController: UICollectionViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .Plain, target: self, action: #selector(newImage))
 
         // Do any additional setup after loading the view.
-        let applicationDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
-        captions = applicationDelegate.captionedImages
+        getImages()
+        captionCollectionView.reloadData()
     }
     
     override func viewWillAppear(animated: Bool) {
-        <#code#>
+         getImages()
+        captionCollectionView.reloadData()
     }
 
     func getImages() {
@@ -37,15 +39,23 @@ class CaptionCollectionViewController: UICollectionViewController {
         let controller = self.storyboard!.instantiateViewControllerWithIdentifier("CreateImage") as! CreateImageViewController
         self.presentViewController(controller, animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return captions.count
     }
-    */
-
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = captionCollectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! CaptionCollectionViewCell
+        let captionedImage = captions[indexPath.item].captionedImage
+        cell.backgroundView = UIImageView(image: captionedImage)
+        return cell
+        
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let detailController = self.storyboard!.instantiateViewControllerWithIdentifier("DetailViewController") as! DetailViewController
+        detailController.currentImage = self.captions[indexPath.item]
+        navigationController!.pushViewController(detailController, animated: true)
+    }
 }
